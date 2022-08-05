4f75cea911671992712ab2da8e30c8cfcd529063
rebeccananono1 <nanonofr@gmail.com>
2022-08-05 09:34:03 +0300

    removed non-gbv audio

--- audio.js
+++ audio.js
@@ -1,7 +1,7 @@
 Amplitude.init({
    "songs":[
       
-
+/*
       {
          "name":"Ugandan Song 1",
          "url":"./audio/MyAudioUgMusic/Ug1.mp3",
@@ -18,6 +18,13 @@ Amplitude.init({
 
       {
          "name":"Ugandan Song 3",
+         "url":"./audio/001.mp3",
+         "source": "Mp3 Jaja",
+         "source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
+      },
+*/ //Old audio commented
+      {
+         "name":"Gender Based Violence Audio 1",
          "url":"./audio/MyAudioUgMusic/Ug3.mp3",
          "source": "Mp3 Jaja",
          "source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"

55aa2c33fd92a21a2bfb884cadff12038a26eb27
rebeccananono1 <nanonofr@gmail.com>
2022-08-05 09:52:55 +0300

    updated names

--- audio.js
+++ audio.js
@@ -25,170 +25,170 @@ Amplitude.init({
 */ //Old audio commented
 
       {
-         "name":"Gender Based Violence Audio 1",
-         "url":"./audio/MyAudioUgMusic/Ug3.mp3",
+         "name":"Nutrition in Luganda Audio 1",
+         "url":"./audio/MyAudioUgMusic/003.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
 
       {
-         "name":"Gender Based Violence Audio 2",
+         "name":"Nutrition in Luganda Audio 2",
          "url":"./audio/002.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 3",
+         "name":"Nutrition in Luganda Audio 3",
          "url":"./audio/003.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 4",
+         "name":"Nutrition in Luganda Audio 4",
          "url":"./audio/004.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 5",
+         "name":"Nutrition in Luganda Audio 5",
          "url":"./audio/005.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 6",
+         "name":"Nutrition in Luganda Audio 6",
          "url":"./audio/006.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 7",
+         "name":"Nutrition in Luganda Audio 7",
          "url":"./audio/008.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 9",
+         "name":"Nutrition in Luganda Audio 9",
          "url":"./audio/009.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 10",
+         "name":"Nutrition in Luganda Audio 10",
          "url":"./audio/010.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 11",
+         "name":"Nutrition in Luganda Audio 11",
          "url":"./audio/011.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 12",
+         "name":"Nutrition in Luganda Audio 12",
          "url":"./audio/012.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 13",
+         "name":"Nutrition in Luganda Audio 13",
          "url":"./audio/013.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 14",
+         "name":"Nutrition in Luganda Audio 14",
          "url":"./audio/014.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 15",
+         "name":"Nutrition in Luganda Audio 15",
          "url":"./audio/015.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 16",
+         "name":"Nutrition in Luganda Audio 16",
          "url":"./audio/016.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 17",
+         "name":"Nutrition in Luganda Audio 17",
          "url":"./audio/017.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 18",
+         "name":"Nutrition in Luganda Audio 18",
          "url":"./audio/018.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 19",
+         "name":"Nutrition in Luganda Audio 19",
          "url":"./audio/019.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 20",
+         "name":"Nutrition in Luganda Audio 20",
          "url":"./audio/020.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 21",
+         "name":"Nutrition in Luganda Audio 21",
          "url":"./audio/021.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 22",
+         "name":"Nutrition in Luganda Audio 22",
          "url":"./audio/022.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 23",
+         "name":"Nutrition in Luganda Audio 23",
          "url":"./audio/023.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 24",
+         "name":"Nutrition in Luganda Audio 24",
          "url":"./audio/024.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 25",
+         "name":"Nutrition in Luganda Audio 25",
          "url":"./audio/025.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 26",
+         "name":"Nutrition in Luganda Audio 26",
          "url":"./audio/026.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 27",
+         "name":"Nutrition in Luganda Audio 27",
          "url":"./audio/027.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 28",
+         "name":"Nutrition in Luganda Audio 28",
          "url":"./audio/028.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
       },
       {
-         "name":"Gender Based Violence Audio 29",
+         "name":"Nutrition in Luganda Audio 29",
          "url":"./audio/029.mp3",
          //"source": "Mp3 Jaja",
          //"source_url": "https://www.mp3jaja.com/profile.php?names=Juliana%20Kanyomozi&id=108"
